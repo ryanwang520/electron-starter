0e64c7638dee2e5a526748f851a4ce7bb3b11e57
Ryan Wang <hwwangwang@gmail.com>
2018-11-11 17:09:49 +0800

    use hook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import { hot } from 'react-hot-loader'
-
 import React from 'react'
 import Counter from './Counter'
 
 const App = () => (
   <div className="App">
     <div>hello parcl</div>
-    <div>hot reload</div>
-    <div>hot reload</div>
+    <div>hot refresh</div>
+    <div>hot reld</div>
     <Counter />
   </div>
 )
 
 export default App
-// export default hot(module)(App)
--- src/Counter.js
+++ src/Counter.js
@@ -1,17 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default class Counter extends React.Component {
-  state = {
-    count: 4,
-  }
-  componentDidMount() {
-    setInterval(() => {
-      this.setState({
-        count: this.state.count + 1,
-      })
-    }, 1000)
-  }
-  render() {
-    return <div>{this.state.count} 110</div>
-  }
+function Example() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  )
 }
+export default Example
